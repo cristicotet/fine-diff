da194656a04629447cf6d28bdb588f6d29f75178
Tobias Knipping <knipping@tk-schulsoftware.de>
2017-06-09 18:31:00 +0200

    update tests

--- tests/Diff/DependencyInjectTest.php
+++ tests/Diff/DependencyInjectTest.php
@@ -2,9 +2,9 @@
 
 namespace FineDiffTests\Diff;
 
-use PHPUnit\Framework\TestCase;
-use Mockery as m;
 use iphis\FineDiff\Diff;
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
 
 class DependencyInjectTest extends TestCase
 {
@@ -16,23 +16,25 @@ class DependencyInjectTest extends TestCase
     public function testGetGranularity()
     {
         $character = m::mock('iphis\FineDiff\Granularity\Character');
-        $character->shouldReceive('justTesting')->once();
+        $character->shouldReceive('justTesting')->once()->andReturnValues([true]);
 
         $diff = new Diff($character);
         $granularity = $diff->getGranularity();
 
-        $granularity->justTesting();
+        $result = $granularity->justTesting();
+        $this->assertSame(true, $result);
     }
 
     public function testGetRenderer()
     {
         $html = m::mock('iphis\FineDiff\Render\Html');
-        $html->shouldReceive('justTesting')->once();
+        $html->shouldReceive('justTesting')->once()->andReturnValues([true]);
 
         $diff = new Diff(null, $html);
         $renderer = $diff->getRenderer();
 
-        $renderer->justTesting();
+        $result = $renderer->justTesting();
+        $this->assertSame(true, $result);
     }
 
     public function testRender()
@@ -44,17 +46,18 @@ class DependencyInjectTest extends TestCase
         $parser->shouldReceive('parse')->andReturn($opcodes);
 
         $html = m::mock('iphis\FineDiff\Render\Html');
-        $html->shouldReceive('process')->with('hello', $opcodes)->once();
-
+        $html->shouldReceive('process')->with('hello', $opcodes)->once()->andReturnValues([true]);
 
         $diff = new Diff(null, $html, $parser);
         $diff->render('hello', 'hello2');
+
+        $this->assertSame(true, $result);
     }
 
     public function testGetParser()
     {
         $parser = m::mock('iphis\FineDiff\Parser\Parser');
-        $parser->shouldReceive('justTesting')->once();
+        $parser->shouldReceive('justTesting')->once()->andReturnValues([true]);
 
         $diff = new Diff(null, null, $parser);
         $parser = $diff->getParser();
@@ -65,7 +68,7 @@ class DependencyInjectTest extends TestCase
     public function testGetOpcodes()
     {
         $parser = m::mock('iphis\FineDiff\Parser\Parser');
-        $parser->shouldReceive('parse')->with('foobar', 'eggfooba')->once();
+        $parser->shouldReceive('parse')->with('foobar', 'eggfooba')->once()->andReturnValues([true]);
 
         $diff = new Diff(null, null, $parser);
         $diff->getOpcodes('foobar', 'eggfooba');
